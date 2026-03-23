60a4f53ef34a6e76c0f8327afb5861996a4eb9c2
Youssef-ElDawayaty <youssefeldawayaty@gmail.com>
2026-03-23 14:21:49 +0200

    Sucssess case

--- train.py
+++ train.py
@@ -7,8 +7,8 @@ with mlflow.start_run() as run:
     run_id = run.info.run_id
     print("Run ID:", run_id)
 
-    accuracy = 0.82  
-    # accuracy = 0.95  
+    # accuracy = 0.82  
+    accuracy = 0.95  
 
 
     mlflow.log_metric("accuracy", accuracy)
